dd8c3bf00998a1de722a06e796e4c250631ff948
HuoYinqi <983673234@qq.com>
2020-06-08 22:45:02 +0800

    update

--- main.c
+++ main.c
@@ -9,19 +9,21 @@ int main()
     int line;
     int start, step;
     Person *people = create_people_from_file("data\\person.txt", &line);
-    for (int i = 0; i < line; i++)
+    printf("people list from file:\n");
+    for(int i = 0; i < line; i++)
     {
         printf("%s\n", get_person_info(people[i]));
     }
-
+    Person p1 = {"New", 10};
     start = 1;
     step = 2;
-
     Josephus jos = create_josephus(start, step, line);
     put_people_in_josephus(&jos, people);
+    josephus_pop(&jos, 2);
+    josephus_append(&jos, p1);
     Person *result = get_result(&jos);
     printf("----------------\n");
-    for (int i = 0; i < line; i++)
+    for (int i = 0; i < jos.number; i++)
     {
         printf("%s\n", get_person_info(result[i]));
     }
