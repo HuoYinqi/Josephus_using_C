91fc7358cc6cc9a3cb6af1120d02b4353b33e594
HuoYinqi <983673234@qq.com>
2020-06-15 09:54:44 +0800

    delete comment

--- src/read_file.c
+++ src/read_file.c
@@ -8,31 +8,6 @@
 #define FALSE 0
 #define OPEN_FILE_FAILED -1
 
-// int read_file(char *path, int *line, char **content)
-// {
-//     content = malloc(5);
-//     FILE *fp = fopen(path, "r");
-//     int i = 0;
-//     char buff[255];
-//     if (fp == NULL)
-//     {
-//         return OPEN_FILE_FAILED;
-//     }
-
-//     while(fgets(buff, 256, fp) != NULL)
-//     {
-//         printf("%d\n", i);
-//         strcpy(content[i], buff);
-//         i++;
-//     }
-
-//     *line = i;
-//     fclose(fp);
-//     return SUCCESS;
-// }
-
-
-
 int create_people_from_file(char* path, int* line, Person *people)
 {   
     int i = 0;
